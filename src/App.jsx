25021216bcb9617e614e6009846ebac353e36c07
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-09-22 10:51:50 +0100

    Modify boilerplate code to suite project needs.
    - installed react query package for server state management and data fetching
    - installed react router for page to page navigation
    - installed tailwindcss and daisy ui for styling and layouts
    - installed dayjs for easier date and time implementation
    - create page components to represent various routes
    - configured browser router
    - added images to the assets folder

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import {
+  Register,
+  Login,
+  HomeLayout,
+  Landing,
+  Products,
+  SingleProduct,
+  Cart,
+  Orders,
+  Checkout,
+  About,
+  Error,
+} from "./pages/index.js";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "products", element: <Products /> },
+      { path: "products/:id", element: <SingleProduct /> },
+      { path: "cart", element: <Cart /> },
+      { path: "about", element: <About /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "orders", element: <Orders /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  { path: "/register", element: <Register /> },
+]);
+
+function App() {
+  return <RouterProvider router={routes}></RouterProvider>;
+}
+
+export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+const HomeLayout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+export default HomeLayout;
