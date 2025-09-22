517410be8958eb4b1024425e37d1734509d9d4f4
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-09-22 12:13:02 +0100

    feat: add registration page layout

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,30 @@
+import { FormInput, SubmitBtn } from "../components";
+import { Form, Link } from "react-router";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="grid h-screen place-items-center">
+      <Form
+        method="post"
+        className="card bg-base-100 flex w-96 flex-col gap-y-4 p-8 shadow-lg"
+      >
+        <h4 className="text-center text-3xl font-bold">Register</h4>
+        <FormInput type={"text"} label={"username"} name={"username"} />
+        <FormInput type={"email"} label={"email"} name={"email"} />
+        <FormInput type={"password"} label={"password"} name={"password"} />
+        <div className="mt-4">
+          <SubmitBtn text={"register"} />
+        </div>
+        <p className="text-center">
+          Already a member?{" "}
+          <Link
+            to={"/login"}
+            className="link link-hover link-primary ml-2 capitalize"
+          >
+            login
+          </Link>
+        </p>
+      </Form>
+    </section>
+  );
 };
 export default Register;
