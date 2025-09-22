7776ea1dc9056ab63331bfea7abd7bad21413063
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-09-22 11:11:44 +0100

    feat: add error handling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,31 @@
+import { Link, useRouteError } from "react-router";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404)
+    return (
+      <main className="grid min-h-screen place-items-center px-8">
+        <div className="text-center">
+          <p className="text-primary text-9xl font-semibold">404</p>
+          <h1 className="fontbold tracking-light mt-4 text-3xl sm:text-5xl">
+            page not found
+          </h1>
+          <p className="mt-6 text-lg leading-7">
+            Sorry, we could not find the page you were looking for.
+          </p>
+          <div className="mt-10">
+            <Link to={`/`} className="btn btn-secondary">
+              go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  return (
+    <main className="grid min-h-screen place-items-center px-8">
+      <h4 className="text-center text-4xl font-bold">there was an error...</h4>
+    </main>
+  );
 };
 export default Error;
