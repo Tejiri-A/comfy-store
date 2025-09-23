89b1e0fff880226949231cb8963f05e2086783e7
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-09-23 16:12:29 +0100

    feat: add utility function to format the price according to US standards
    - Change the title in the index html file

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { Link, useLoaderData } from "react-router";
+import { formatPrice } from "../utils/index.js";
 
 const ProductsGrid = () => {
   const { products } = useLoaderData();
@@ -7,8 +8,10 @@ const ProductsGrid = () => {
     <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => {
         const { title, price, image } = product.attributes;
+        const dollarsAmount = formatPrice(price);
         return (
           <Link
+            key={product.id}
             to={`/products/${product.id}`}
             className="card w-full shadow-xl transition duration-300 hover:shadow-2xl"
           >
@@ -21,7 +24,7 @@ const ProductsGrid = () => {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title tracking-wider">{title}</h2>
-              <span className="text-secondary">{price}</span>
+              <span className="text-secondary">{dollarsAmount}</span>
             </div>
           </Link>
         );
