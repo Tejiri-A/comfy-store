e98f26819e6c036940b354308644fa667a15026d
Oghenetejiri Samuel Amrasa <tjthecreator8@gmail.com>
2025-09-30 10:47:37 +0100

    Revert "feat(state): initialized redux store for global state management"
    This reverts commit 8208f9b5ae757e2010e8a651aa32d6e6631154f8.

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./features/cart/cartSlice.js";
-export const store = configureStore({
-  reducer: {
-    cartState: cartReducer,
-  },
-});
